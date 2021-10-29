2b1d6a61279d98c2de2480b7f2b954fb1aed9599
suresh ponnada <suresh.ponnada@v2solutions.com>
2021-10-29 15:10:41 +0530

    This is a master branch changes!!

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,7 +2,7 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <p>
-      This is a master branch!!
+      This is a master branch changes!!
     </p>
     <h3>Installed CLI Plugins</h3>
     <ul>
